80b88c576bf58c6e7902cc4b1e3612baac25d823
Kothakapu Pooja <poojareddy5588@gmail.com>
2026-09-21 12:33:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here!
 
+//your code here!
+let list = document.getElementById('infi-list')
+const addItems = (count) => {
+	for(let i = 0; i < count; i++){
+		let li = document.createElement('li')
+		li.textContent = "Item" + (list.children.length  + 1)
+		list.appendChild(li)
+	}
+}
+addItems(10)
+
+list.addEventListener("scroll",()=>{
+	if(list.scrollTop + list.clientHeight >= list.scrollHeight){
+		addItems(2)
+	}
+})
